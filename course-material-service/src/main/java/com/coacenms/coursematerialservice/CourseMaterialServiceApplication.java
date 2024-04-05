8249058f376340cc02af83313d5a40d9a50bb0b8
Divakarsbhat01 <divakarsbhat@gmail.com>
2024-04-05 19:39:22 +0530

    "Added more services"

---
+++ course-material-service/src/main/java/com/coacenms/coursematerialservice/CourseMaterialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coacenms.coursematerialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMaterialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMaterialServiceApplication.class, args);
+	}
+
+}
---
+++ course-material-service/src/test/java/com/coacenms/coursematerialservice/CourseMaterialServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coacenms.coursematerialservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMaterialServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
